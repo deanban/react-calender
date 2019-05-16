3e6a72fb095af579c48c1e103a2138ac821244f9
deanban <pbp2005@gmail.com>
2019-05-16 13:31:50 -0400

    calendar working with gapi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const [gapiRdy, setGapiRdy] = useState(false);
+  const [events, setEvents] = useState([]);
+
+  const GOOGLE_API_KEY = 'AIzaSyBjvvNhVuFHqn2xahUw7cELBXlItsjvcu4';
+  const CALENDAR_ID = 'gpoeuf4afk2plvmpbkc9r184rk@group.calendar.google.com';
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  const getEvents = () => {
+    function start() {
+      window.gapi.client
+        .init({
+          apiKey: GOOGLE_API_KEY
+        })
+        .then(function() {
+          return window.gapi.client.request({
+            path: `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events`
+          });
+        })
+        .then(response => {
+          console.log(response);
+          setEvents(response.result.items);
+        });
+    }
+    window.gapi.load('client', start);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
